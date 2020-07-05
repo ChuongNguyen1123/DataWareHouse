1c78f64cc5826a6ce2f18e00c1d7414d5814386d
justnct <67194210+justnct@users.noreply.github.com>
2020-07-05 23:47:45 +0700

    NguyenChiThien_DataWarehouse

--- Nhom12_Datawarehouse/src/data_warehouse/DataProcessing.java
+++ Nhom12_Datawarehouse/src/data_warehouse/DataProcessing.java
@@ -6,56 +6,56 @@ import java.sql.*;
 import connectionDB.ConnectionDB;
 
 public class DataProcessing {
-//	static String DB_URL = "jdbc:mysql://localhost:3306/student";
-//	static String USER_NAME = "root";
-//	static String PASSWORD = "";
+	static String NAME_DB = "student";
 	static String LIST_DB = "name_of_database";
 	static String DB_DES = "newstudent";
 	ConnectionDB connect = new ConnectionDB();
-	
 
 	// *We get so much DB in MySql to copy.
 	// *Add name of DB to another Database
 	public void addNameData() throws SQLException, IOException, ClassNotFoundException {
-	    Connection  connection = connect.loadProps();
-		DatabaseMetaData dbmd = connection.getMetaData();
-		String table[] = {};
-		ResultSet rs = dbmd.getTables(null, null, null, table);
-		PreparedStatement ps_copyNameToAnotherDB = connection.prepareStatement("Insert into" + LIST_DB + "values(?)");
-		PreparedStatement ps_checkDB = connection.prepareStatement("Select Name from " + LIST_DB);
-		ResultSet rs_checkDB = ps_checkDB.executeQuery();
-		// *Cannot add yourself and duplicate database
-		while (rs.next()) {
-			while (rs_checkDB.next()) {
-				if (!rs.getString(3).equalsIgnoreCase(rs_checkDB.getString(1)) && !rs.getString(3).equals(LIST_DB)
-						&& !rs.getString(3).equals(DB_DES)) {
-					ps_copyNameToAnotherDB.setString(1, rs.getString(3));
+		Connection connection = connect.loadProps();
+		// *get all name in database
+		PreparedStatement ps_showTable = connection.prepareStatement("SELECT table_name FROM information_schema.tables WHERE table_schema = '" + NAME_DB + "'");
+		ResultSet rs_showTable = ps_showTable.executeQuery();
+		// *get Name of table from ListDB to compare
+		PreparedStatement ps_getNameInListDBToCompare = connection.prepareStatement("SELECT Name From " + LIST_DB);
+		ResultSet rs_getNameInListDBToCompare = ps_getNameInListDBToCompare.executeQuery();
+		while (rs_showTable.next()) {
+//			while (rs_getNameInListDBToCompare.next()) {
+				 //*don't be the same with List database and database des
+				
+				if ( !rs_showTable.getString(1).equalsIgnoreCase(DB_DES) && !rs_showTable.getString(1).equalsIgnoreCase(LIST_DB)) {
+					// *add into listDB
+					PreparedStatement ps_copyNameToAnotherDB = connection.prepareStatement("Insert into " + LIST_DB + " values(?)");
+					ps_copyNameToAnotherDB.setString(1, rs_showTable.getString(1));
 					ps_copyNameToAnotherDB.executeUpdate();
 				}
-			}
+			
 		}
-	}
+		}
+//	}
 
 	// *Check col at first if col != between source and des it is wrong.
 	public void checkSourcDB() throws SQLException, IOException, ClassNotFoundException {
-	    Connection  connection = connect.loadProps();
-		//select name of database
+		Connection connection = connect.loadProps();
+		// select name of database
 		PreparedStatement ps_getNameOfListDB = connection.prepareStatement("Select Name from " + LIST_DB);
 		ResultSet rs_getNameOfListDB = ps_getNameOfListDB.executeQuery();
-		//select name
+		// select name
 		while (rs_getNameOfListDB.next()) {
 			// get data from name database
 			PreparedStatement ps_getDBFromList = connection
 					.prepareStatement("Select * from " + rs_getNameOfListDB.getString(1));
 			ResultSet rs_getDBFromList = ps_getDBFromList.executeQuery();
-			//*create object to count col of database source
+			// *create object to count col of database source
 			ResultSetMetaData rsmd1 = rs_getDBFromList.getMetaData();
 			int numOfCol1 = rsmd1.getColumnCount();
 
 			// get col database Des
 			PreparedStatement ps_getDBDes = connection.prepareStatement("Select * from " + DB_DES);
 			ResultSet rs_getDBDes = ps_getDBDes.executeQuery();
-			//*create object to count col of database source
+			// *create object to count col of database source
 			ResultSetMetaData rsmd2 = rs_getDBDes.getMetaData();
 			int numOfCol2 = rsmd2.getColumnCount();
 			if (numOfCol1 == numOfCol2) {
@@ -63,31 +63,35 @@ public class DataProcessing {
 				String nameOfCol2 = "";
 				String ColTypeName1 = "";
 				String ColTypeName2 = "";
-				//count col same(name,type)
+				// count col same(name,type)
 				int count = 0;
 				for (int i = 1; i < numOfCol1 + 1; i++) {
-					//*get name of col
+					// *get name of col
 					nameOfCol1 = rsmd1.getColumnName(i);
 					nameOfCol2 = rsmd2.getColumnName(i);
-					//*get type of col
+					// *get type of col
 					ColTypeName1 = rsmd1.getColumnTypeName(i);
 					ColTypeName2 = rsmd2.getColumnTypeName(i);
 					if (nameOfCol1.equals(nameOfCol2) && ColTypeName1.equals(ColTypeName2)) {
 						count++;
 					}
 				}
-				//*active = 0 is not approved
-				//*active = 1 is can copy
-				//*active = 2 is can't copy
+				// *don't have active is not approved
+				// *active = 1 is can copy
+				// *active = 2 is can't copy
 				if (count == numOfCol1) {
-					PreparedStatement ps_createActive1 = connection.prepareStatement("ALTER TABLE " + rs_getNameOfListDB.getString(1) +" ADD Active int(1) NOT NULL");
+					PreparedStatement ps_createActive1 = connection.prepareStatement(
+							"ALTER TABLE " + rs_getNameOfListDB.getString(1) + " ADD Active int(1) NOT NULL");
 					ps_createActive1.executeUpdate();
-					PreparedStatement ps_setActive1 = connection.prepareStatement("UPDATE " + rs_getNameOfListDB.getString(1) +" SET Active=1");
+					PreparedStatement ps_setActive1 = connection
+							.prepareStatement("UPDATE " + rs_getNameOfListDB.getString(1) + " SET Active=1");
 					ps_setActive1.executeUpdate();
-				}else{
-					PreparedStatement ps_createActive2 = connection.prepareStatement("ALTER TABLE " + rs_getNameOfListDB.getString(1) +" ADD Active int(1) NOT NULL");
+				} else {
+					PreparedStatement ps_createActive2 = connection.prepareStatement(
+							"ALTER TABLE " + rs_getNameOfListDB.getString(1) + " ADD Active int(1) NOT NULL");
 					ps_createActive2.executeUpdate();
-					PreparedStatement ps_setActive2 = connection.prepareStatement("UPDATE " + rs_getNameOfListDB.getString(1) +" SET Active=2");
+					PreparedStatement ps_setActive2 = connection
+							.prepareStatement("UPDATE " + rs_getNameOfListDB.getString(1) + " SET Active=2");
 					ps_setActive2.executeUpdate();
 				}
 			}
@@ -95,17 +99,17 @@ public class DataProcessing {
 	}
 
 	public void createTable(String name) throws SQLException, IOException, ClassNotFoundException {
-	    Connection  connection = connect.loadProps();
+		Connection connection = connect.loadProps();
 		String ct = "CREATE TABLE " + name
-				+ "( Stt int NOT NULL PRIMARY KEY AUTO_INCREMENT, MaSV VARCHAR(20), Ho VARCHAR(20), Ten VARCHAR(20), NgaySinh VARCHAR(20), MaLop VARCHAR(20), TenLop VARCHAR(20), SoDT int, Email VARCHAR(20), QueQuan VARCHAR(20), GhiChu VARCHAR(20))";
+				+ "( Stt int NOT NULL PRIMARY KEY AUTO_INCREMENT,MaSV VARCHAR(20), Ho VARCHAR(20), Ten VARCHAR(20), NgaySinh VARCHAR(20), MaLop VARCHAR(20), TenLop VARCHAR(20), SoDT int, Email VARCHAR(20), QueQuan VARCHAR(20), GhiChu VARCHAR(20))";
 		System.out.println(ct);
 		PreparedStatement ps_createTable = connection.prepareStatement(ct);
 		ps_createTable.executeUpdate();
 	}
 
 	public void copyFromThisDBToAnotherDB() throws SQLException, IOException, ClassNotFoundException {
-	    Connection  connection = connect.loadProps();
-		//*Select name of Database
+		Connection connection = connect.loadProps();
+		// *Select name of Database
 		PreparedStatement ps_getNameOfListDB = connection.prepareStatement("Select Name from " + LIST_DB);
 		ResultSet rs_getNameOfListDB = ps_getNameOfListDB.executeQuery();
 		while (rs_getNameOfListDB.next()) {
@@ -113,8 +117,9 @@ public class DataProcessing {
 			PreparedStatement ps_getDBFromList = connection
 					.prepareStatement("Select * From " + rs_getNameOfListDB.getString(1) + " WHERE Active=1");
 			ResultSet rs_getDBFromList = ps_getDBFromList.executeQuery();
-			PreparedStatement ps_copy= connection.prepareStatement("Insert into " + DB_DES +" VALUES(?,?,?,?,?,?,?,?,?,?,?)");
-			while(rs_getDBFromList.next()){
+			PreparedStatement ps_copy = connection
+					.prepareStatement("Insert into " + DB_DES + " VALUES(?,?,?,?,?,?,?,?,?,?,?)");
+			while (rs_getDBFromList.next()) {
 				ps_copy.setInt(1, 0);
 				ps_copy.setString(2, rs_getDBFromList.getString(2));
 				ps_copy.setString(3, rs_getDBFromList.getString(3));
@@ -126,17 +131,19 @@ public class DataProcessing {
 				ps_copy.setString(9, rs_getDBFromList.getString(9));
 				ps_copy.setString(10, rs_getDBFromList.getString(10));
 				ps_copy.setString(11, rs_getDBFromList.getString(11));
-				ps_copy.executeUpdate();
-				System.out.println("Success");
+				ps_copy.execute();
+				PreparedStatement ps_setActive3 = connection.prepareStatement("UPDATE " + rs_getNameOfListDB.getString(1) + " SET Active=3");
+				ps_setActive3.executeUpdate();
+				
 			}
 		}
 	}
 
 	public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
 		DataProcessing dp = new DataProcessing();
-//		dp.createTable("newstudent");
+//		 dp.createTable("student1");
 		dp.addNameData();
-		dp.checkSourcDB();
-		dp.copyFromThisDBToAnotherDB();
+//		dp.checkSourcDB();
+//		dp.copyFromThisDBToAnotherDB();
 	}
 }
