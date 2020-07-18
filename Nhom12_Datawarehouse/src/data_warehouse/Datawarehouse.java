95427e3fc8b08640e0dfc6e879f5cf5e195f729b
justnct <67194210+justnct@users.noreply.github.com>
2020-07-18 11:47:36 +0700

    NCT-Datawarehouse

--- Nhom12_Datawarehouse/src/data_warehouse/Datawarehouse.java
+++ Nhom12_Datawarehouse/src/data_warehouse/Datawarehouse.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 import connectionDB.ConnectionDB;
@@ -18,26 +19,36 @@ public class Datawarehouse {
 	// *connection connect to Control
 	// *connection1 connect to Staging
 	// *connection2 connect to Datawarehouse
+
 	public void createDatabase() throws ClassNotFoundException, IOException, SQLException {
-		// *Connect to Control
 		Connection connection = connect.loadProps();
+		// *Connect to Control
 		// *Connect to Staging
 		Connection connection1 = connect1.loadProps();
 		// *Connect to Datawarehouse
 		Connection connection2 = connect2.loadProps();
-		PreparedStatement ps_getTableName = connection
-				.prepareStatement("Select table_name_datawarehouse,field_name from my_config where isActive=1");
-		ResultSet rs_getTableName = ps_getTableName.executeQuery();
-		while (rs_getTableName.next()) {
+		// *Get source and des
+		PreparedStatement ps_getFolderName = connection
+				.prepareStatement("Select source_folder,folder_local from table_config");
+		ResultSet rs_getFolderName = ps_getFolderName.executeQuery();
+		while (rs_getFolderName.next()) {
+			// *Get all in source
+			PreparedStatement ps_getFromSource = connection1
+					.prepareStatement("Select * from " + rs_getFolderName.getString(1));
+			ResultSet rs_getFromSource = ps_getFromSource.executeQuery();
+			ResultSetMetaData rsmd_getFromSoure = rs_getFromSource.getMetaData();
+			// *Get num of col in table source
+			int num_of_col = rsmd_getFromSoure.getColumnCount();
+			// *Create sql to create database
 			String sql = "Create table ";
-			sql += rs_getTableName.getString(1) + "(";
-			String[] result = rs_getTableName.getString(2).split(",");
-			for (int i = 0; i < result.length; i++) {
-				if (i == result.length - 1) {
-					sql += result[i] + " varchar(255))";
+			sql += rs_getFolderName.getString(2) + "(";
+			for (int i = 1; i < num_of_col + 1; i++) {
+				if (i == num_of_col) {
+					sql += rsmd_getFromSoure.getColumnName(i) + " varchar(255))";
 					break;
 				}
-				sql += result[i] + " varchar(255),";
+				String col_name = rsmd_getFromSoure.getColumnName(i);
+				sql += col_name + " varchar(255),";
 			}
 			PreparedStatement ps_createDB = connection2.prepareStatement(sql);
 			ps_createDB.executeUpdate();
@@ -49,25 +60,58 @@ public class Datawarehouse {
 		Connection connection = connect.loadProps();
 		Connection connection1 = connect1.loadProps();
 		Connection connection2 = connect2.loadProps();
-		PreparedStatement ps_getTable_Name = connection.prepareStatement(
-				"Select table_name_staging,table_name_datawarehouse,insert_DW_sql,table_column from my_config ");
+
+		PreparedStatement ps_getTable_Name = connection
+				.prepareStatement("Select id,source_folder,folder_local from table_config ");
 		ResultSet rs_getTable_Name = ps_getTable_Name.executeQuery();
 		while (rs_getTable_Name.next()) {
-			PreparedStatement ps_getStatus = connection
-					.prepareStatement("Select status from log where name = '" + rs_getTable_Name.getString(1) + "'");
+			// *Get status of log
+			PreparedStatement ps_getStatus = connection.prepareStatement(
+					"Select status from table_log where id = '" + rs_getTable_Name.getString(1) + "'");
 			ResultSet rs_getStatus = ps_getStatus.executeQuery();
 			while (rs_getStatus.next()) {
-				if (rs_getStatus.getString(1).equalsIgnoreCase("OK")) {
+				// *Check status
+				if (rs_getStatus.getString(1).equals("Download_OK")) {
+
+					// *Get all of staging
 					PreparedStatement ps_getFromStaging = connection1
-							.prepareStatement("Select * from " + rs_getTable_Name.getString(1));
+							.prepareStatement("Select * from " + rs_getTable_Name.getString(2));
 					ResultSet rs_getFromStaging = ps_getFromStaging.executeQuery();
-					while (rs_getFromStaging.next()) {
-						PreparedStatement ps_insertToDB = connection2.prepareStatement(rs_getTable_Name.getString(3));
-						for (int i = 0; i < rs_getTable_Name.getInt(4); i++) {
-							ps_insertToDB.setString(i+1, rs_getFromStaging.getString(i+1));
+					ResultSetMetaData rsmd_getFromStaging = rs_getFromStaging.getMetaData();
+					int colOfStaging = rsmd_getFromStaging.getColumnCount();
+
+					// *Get all of DW
+					PreparedStatement ps_getFromDW = connection2
+							.prepareStatement("Select * from " + rs_getTable_Name.getString(3));
+					ResultSet rs_getFromDW = ps_getFromDW.executeQuery();
+					ResultSetMetaData rsmd_getFromDW = rs_getFromDW.getMetaData();
+					int colOfDW = rsmd_getFromDW.getColumnCount();
+					// *Check col of 2 database
+					if (colOfDW == colOfStaging) {
+						String sql = "Insert into " + rs_getTable_Name.getString(3) + " values(";
+						for (int i = 1; i < colOfDW + 1; i++) {
+							if (i == colOfDW) {
+								sql += "?)";
+								break;
+							}
+							sql += "?,";
 						}
-						ps_insertToDB.executeUpdate();
+						while (rs_getFromStaging.next()) {
+							PreparedStatement ps_insertToDB = connection2.prepareStatement(sql);
+							for (int i = 1; i < colOfDW + 1; i++) {
+								ps_insertToDB.setString(i, rs_getFromStaging.getString(i));
+							}
+							ps_insertToDB.executeUpdate();
+							PreparedStatement ps_delete = connection1.prepareStatement("Delete from "
+									+ rs_getTable_Name.getString(2) + " where STT=" + rs_getFromStaging.getString(1));
+							ps_delete.executeUpdate();
+							// send mail success
+						}
+					} else {
+						// *Send mail fail because not same number of column
 					}
+				} else {
+					// *Send mail fail because source is not dowload_OK
 				}
 
 			}
