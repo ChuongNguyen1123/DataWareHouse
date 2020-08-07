dd86dcd8ac2be633b9964c54bb9e0f50656093e3
justnct <67194210+justnct@users.noreply.github.com>
2020-08-07 22:17:19 +0700

    CopyToWarehouse

--- Nhom12_Datawarehouse/src/data_warehouse/CopyToWarehouse.java
+++ Nhom12_Datawarehouse/src/data_warehouse/CopyToWarehouse.java
@@ -1,5 +1,6 @@
 package data_warehouse;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Date;
@@ -17,114 +18,154 @@ public class CopyToWarehouse {
 	ConnectionDB connect = new ConnectionDB();
 	ConnectionDB1 connect1 = new ConnectionDB1();
 	ConnectionDB2 connect2 = new ConnectionDB2();
+	Properties pros = new Properties();
 
 	public void copy(int id) throws IOException, SQLException, ClassNotFoundException {
 		Connection connection = connect.loadProps();
 		Connection connection1 = connect1.loadProps();
 		Connection connection2 = connect2.loadProps();
+		FileInputStream f = new FileInputStream("src/data_warehouse/dms_config.properties");
+		pros.load(f);
+		final String DATABASE_2 = pros.getProperty("database2");
+		final String DATABASE_3 = pros.getProperty("database3");
 		// Lay du lieu tu config
 		PreparedStatement ps_getDataFromConfig = connection
 				.prepareStatement("SELECT table_name,table_warehouse FROM table_config where id = " + id);
 		ResultSet rs_getDataFromConfig = ps_getDataFromConfig.executeQuery();
 		while (rs_getDataFromConfig.next()) {
-			// Lay het cac du lieu trong staging
-			PreparedStatement ps_getAllFromStaging = connection1
-					.prepareStatement("SELECT * FROM " + rs_getDataFromConfig.getString(1));
-			ResultSet rs_getAllFromStaging = ps_getAllFromStaging.executeQuery();
-			// Lay ra so cot cua table trong staging
-			ResultSetMetaData rsmd_getAllFromStaging = rs_getAllFromStaging.getMetaData();
-			int countColOfStaging = rsmd_getAllFromStaging.getColumnCount();
+			// Kiem tra ton tai table trong staging
+			boolean existInStaging = false;
+			// Lay het cac table trong staging
+			PreparedStatement ps_checkTableInStaging = connection1.prepareStatement(
+					"SELECT TABLE_NAME FROM information_schema.TABLES WHERE table_schema = '" + DATABASE_2 + "'");
+			ResultSet rs_checkTableInStaging = ps_checkTableInStaging.executeQuery();
+			while (rs_checkTableInStaging.next()) {
+				//Kiem tra ten table trong staging lay trong config co ton tai trong database staging
+				if (rs_getDataFromConfig.getString(1).equals(rs_checkTableInStaging.getString(1))) {
+					existInStaging = true;
+					break;
+				}
+			}
+			// Kiem tra ton tai table trong DW
+			boolean existInDW = false;
+			// Lay het cac table trong staging
+			PreparedStatement ps_checkTableInDW = connection2.prepareStatement(
+					"SELECT TABLE_NAME FROM information_schema.TABLES WHERE table_schema = '" + DATABASE_3 + "'");
+			ResultSet rs_checkTableInDW = ps_checkTableInDW.executeQuery();
+			while (rs_checkTableInDW.next()) {
+				//Kiem tra ten table trong DW lay trong config co ton tai trong database DW
+				if (rs_getDataFromConfig.getString(2).equals(rs_checkTableInDW.getString(1))) {
+					existInDW = true;
+					break;
+				}
+			}
+			// Neu ca 2 table deu ton tai
+			if (existInDW == true && existInStaging == true) {
+				// Lay het cac du lieu trong table staging lay tu config
+				PreparedStatement ps_getAllFromStaging = connection1
+						.prepareStatement("SELECT * FROM " + rs_getDataFromConfig.getString(1));
+				ResultSet rs_getAllFromStaging = ps_getAllFromStaging.executeQuery();
+				// Lay ra so cot cua table trong staging
+				ResultSetMetaData rsmd_getAllFromStaging = rs_getAllFromStaging.getMetaData();
+				int countColOfStaging = rsmd_getAllFromStaging.getColumnCount();
 
-			// Lay het cac du lieu trong DW
-			PreparedStatement ps_getAllFromDW = connection2
-					.prepareStatement("SELECT * FROM " + rs_getDataFromConfig.getString(2));
-			ResultSet rs_getAllFromDW = ps_getAllFromDW.executeQuery();
-			// Lay ra so cot cua table trong DW
-			ResultSetMetaData rsmd_getAllFromDW = rs_getAllFromDW.getMetaData();
-			int countColOfDW = rsmd_getAllFromDW.getColumnCount();
-			// Neu 2 cot cung so cot thi tinh tiep
-			if (countColOfDW == countColOfStaging) {
-				// Kiem tra ten cot
-				int count = 0;// Bien kiem tra trung ten cot
-				for (int i = 1; i < countColOfDW + 1; i++) {
-					if (rsmd_getAllFromDW.getColumnName(i).equals(rsmd_getAllFromStaging.getColumnName(i))) {
-						count++;
-					}
-				}
-				// Neu nhu tat ca cac cot trung nhau
-				if (count == countColOfDW) {
-					// Tao cau lenh sql de copy
-					String sql = "INSERT INTO " + rs_getDataFromConfig.getString(2) + " VALUES(";
-					for (int i = 0; i < count; i++) {
-						if (i == count - 1) {
-							sql += "?)";
-							break;
+				// Lay het cac du lieu trong table DW lay tu config
+				PreparedStatement ps_getAllFromDW = connection2
+						.prepareStatement("SELECT * FROM " + rs_getDataFromConfig.getString(2));
+				ResultSet rs_getAllFromDW = ps_getAllFromDW.executeQuery();
+				// Lay ra so cot cua table trong DW
+				ResultSetMetaData rsmd_getAllFromDW = rs_getAllFromDW.getMetaData();
+				int countColOfDW = rsmd_getAllFromDW.getColumnCount();
+				// Neu 2 cot cung so cot thi tinh tiep
+				if (countColOfDW == countColOfStaging) {
+					// Kiem tra ten cot
+					int count = 0;// Bien kiem tra trung ten cot
+					for (int i = 1; i < countColOfDW + 1; i++) {
+						if (rsmd_getAllFromDW.getColumnName(i).equals(rsmd_getAllFromStaging.getColumnName(i))) {
+							count++;
 						}
-						sql += "?,";
 					}
-					// Copy
-					// Lay du lieu tu staging
-					while (rs_getAllFromStaging.next()) {
-						PreparedStatement ps_copyToDW = connection2.prepareStatement(sql);
-						for (int i = 1; i < count + 1; i++) {
-							// Neu cot co kieu du lieu la int
-							if (rsmd_getAllFromDW.getColumnTypeName(i).contains("INT")) {
-								// Neu du lieu trong
-								if (rs_getAllFromStaging.getString(i) == null
-										|| rs_getAllFromStaging.getString(i).equals("")
-										|| rs_getAllFromStaging.getString(i).equals(" ")) {
-									ps_copyToDW.setInt(i, 0);
-								} else {
-									ps_copyToDW.setInt(i, convertToInt(rs_getAllFromStaging.getString(i)));
-								}
+					// Neu nhu tat ca cac cot trung nhau
+					if (count == countColOfDW) {
+						// Tao cau lenh sql de copy
+						String sql = "INSERT INTO " + rs_getDataFromConfig.getString(2) + " VALUES(";
+						for (int i = 0; i < count; i++) {
+							if (i == count - 1) {
+								sql += "?)";
+								break;
 							}
-							// Neu cot co kieu du lieu la varchar
-							if (rsmd_getAllFromDW.getColumnTypeName(i).contains("VARCHAR")) {
-								// Neu du lieu trong
-								if (rs_getAllFromStaging.getString(i) == null
-										|| rs_getAllFromStaging.getString(i).equals("")
-										|| rs_getAllFromStaging.getString(i).equals(" ")) {
-									ps_copyToDW.setString(i, "Null");
-								} else {
-									ps_copyToDW.setString(i, rs_getAllFromStaging.getString(i));
-								}
-							}
-							// Neu cot co kieu du lieu la date
-							if (rsmd_getAllFromDW.getColumnTypeName(i).contains("DATE")) {
-								// Neu du lieu trong
-//								System.out.println(convertToYMD(rs_getAllFromStaging.getString(i)));
-								if (rs_getAllFromStaging.getString(i) == null
-										|| rs_getAllFromStaging.getString(i).equals("")
-										|| rs_getAllFromStaging.getString(i).equals(" ")) {
-									ps_copyToDW.setDate(i, convertToDate("1970-1-1"));
-								} else {
-									ps_copyToDW.setDate(i,
-											convertToDate(convertToYMD(rs_getAllFromStaging.getString(i))));
-								}
-							}
+							sql += "?,";
 						}
-						ps_copyToDW.executeUpdate();
-						// Xoa trong staging
-						PreparedStatement ps_deleteInStaging = connection1
-								.prepareStatement("Delete from " + rs_getDataFromConfig.getString(1) + " where "
-										+ rsmd_getAllFromStaging.getColumnName(1) + " ='"
-										+ rs_getAllFromStaging.getInt(1) + "'");
-						ps_deleteInStaging.executeUpdate();
+						// Copy
+						// Lay du lieu tu staging
+						while (rs_getAllFromStaging.next()) {
+							//Thuc hien insert vao DW
+							PreparedStatement ps_copyToDW = connection2.prepareStatement(sql);
+							for (int i = 1; i < count + 1; i++) {
+								// Neu cot co kieu du lieu la int
+								if (rsmd_getAllFromDW.getColumnTypeName(i).contains("INT")) {
+									// Neu du lieu trong
+									if (rs_getAllFromStaging.getString(i) == null
+											|| rs_getAllFromStaging.getString(i).equals("")
+											|| rs_getAllFromStaging.getString(i).equals(" ")) {
+										//Cho mac dinh la 0
+										ps_copyToDW.setInt(i, 0);
+									} else {
+										ps_copyToDW.setInt(i, convertToInt(rs_getAllFromStaging.getString(i)));
+									}
+								}
+								// Neu cot co kieu du lieu la varchar
+								if (rsmd_getAllFromDW.getColumnTypeName(i).contains("VARCHAR")) {
+									// Neu du lieu trong
+									if (rs_getAllFromStaging.getString(i) == null
+											|| rs_getAllFromStaging.getString(i).equals("")
+											|| rs_getAllFromStaging.getString(i).equals(" ")) {
+										//Cho mac dinh la Null
+										ps_copyToDW.setString(i, "Null");
+									} else {
+										ps_copyToDW.setString(i, rs_getAllFromStaging.getString(i));
+									}
+								}
+								// Neu cot co kieu du lieu la date
+								if (rsmd_getAllFromDW.getColumnTypeName(i).contains("DATE")) {
+									// Neu du lieu trong
+									if (rs_getAllFromStaging.getString(i) == null
+											|| rs_getAllFromStaging.getString(i).equals("")
+											|| rs_getAllFromStaging.getString(i).equals(" ")) {
+										//Cho mac dinh la ngay 1970-1-1
+										ps_copyToDW.setDate(i, convertToDate("1970-1-1"));
+									} else {
+										//Chuyen ve kieu date trong sql dung dang Y-M-D
+										ps_copyToDW.setDate(i,
+												convertToDate(convertToYMD(rs_getAllFromStaging.getString(i))));
+									}
+								}
+							}
+							ps_copyToDW.executeUpdate();
+							// Xoa trong staging
+							PreparedStatement ps_deleteInStaging = connection1
+									.prepareStatement("Delete from " + rs_getDataFromConfig.getString(1) + " where "
+											+ rsmd_getAllFromStaging.getColumnName(1) + " ='"
+											+ rs_getAllFromStaging.getInt(1) + "'");
+							ps_deleteInStaging.executeUpdate();
+						}
+						// Update copy thanh cong len isSuccess
+						PreparedStatement ps_setSuccess = connection
+								.prepareStatement("UPDATE table_config set is_Success ='Success' where id=" + id);
+						ps_setSuccess.executeUpdate();
+						// Update thoi gian copy
+						PreparedStatement ps_setTimeUpdate = connection.prepareStatement(
+								"UPDATE table_config set timeCopy  ='" + getTime() + "' " + "where id=" + id);
+						ps_setTimeUpdate.executeUpdate();
+						System.out.println("Success");
+					} else {
+						System.out.println("Khong copy vi khong trung ten cot");
 					}
-					// Update copy thanh cong len isSuccess
-					PreparedStatement ps_setSuccess = connection
-							.prepareStatement("UPDATE table_config set is_Success ='Success' where id=" + id);
-					ps_setSuccess.executeUpdate();
-					// Update thoi gian copy
-					PreparedStatement ps_setTimeUpdate = connection.prepareStatement(
-							"UPDATE table_config set timeCopy  ='" + getTime() + "' " + "where id=" + id);
-					ps_setTimeUpdate.executeUpdate();
-					System.out.println("Success");
 				} else {
-					System.out.println("Khong copy vi khong trung ten cot");
+					System.out.println("Khong copy vi khong cung so luong cot");
 				}
-			} else {
-				System.out.println("Khong copy vi khong cung so luong cot");
+			}else {
+				System.out.println("Table khong ton tai");
 			}
 		}
 	}
@@ -144,7 +185,7 @@ public class CopyToWarehouse {
 				first += s;
 				break;
 			}
-			if (Integer.parseInt(first) > 1900) {
+			if (Integer.parseInt(first) > 31) {
 				time.replaceAll("/", "-");
 				return time;
 			} else {
@@ -172,7 +213,7 @@ public class CopyToWarehouse {
 				first += s;
 				break;
 			}
-			if (Integer.parseInt(first) > 1900) {
+			if (Integer.parseInt(first) > 31) {
 				return time;
 			} else {
 				String day = "";
@@ -208,12 +249,12 @@ public class CopyToWarehouse {
 	}
 
 	public static void main(String[] args) throws ClassNotFoundException, IOException, SQLException {
-		CopyToWarehouse d = new CopyToWarehouse();
+		CopyToWarehouse cptdw = new CopyToWarehouse();
 		while (true) {
 			System.out.println("Nhap id:");
 			Scanner sc = new Scanner(System.in);
 			int id = sc.nextInt();
-			d.copy(id);
+			cptdw.copy(id);
 		}
 	}
 }
